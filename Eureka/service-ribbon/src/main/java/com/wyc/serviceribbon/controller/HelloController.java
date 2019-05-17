6d4541840993c6ecf13fa1a71ade0c72a0f277fc
anymore <869609651@qq.com>
2019-05-17 15:16:30 +0800

    initial project

---
+++ Eureka/service-ribbon/src/main/java/com/wyc/serviceribbon/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.wyc.serviceribbon.controller;
+
+import com.wyc.serviceribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping("hello")
+    public String hello(String name){
+        return helloService.helloService(name);
+    }
+}
---
+++ Eureka/service-ribbon/src/main/java/com/wyc/serviceribbon/service/HelloService.java
@@ -0,0 +1,16 @@
+package com.wyc.serviceribbon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String helloService(String name){
+        return restTemplate.getForObject("http://service-hello/hello?name="+name, String.class);
+    }
+}
+
