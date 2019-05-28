5fd5b5a856825804f241a894304535453d90c1d9
anymore <869609651@qq.com>
2019-05-28 14:38:53 +0800

    Feign、Hystrix、zuul

--- Eureka/service-ribbon/src/main/java/com/wyc/serviceribbon/ServiceRibbonApplication.java
+++ Eureka/service-ribbon/src/main/java/com/wyc/serviceribbon/ServiceRibbonApplication.java
@@ -2,10 +2,12 @@ package com.wyc.serviceribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +15,9 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableHystrix//断路器
+
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ServiceRibbonApplication {
 
     public static void main(String[] args) {
